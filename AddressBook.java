88ca54292c0fbc64e00162bd6a93f0bd9ff56931
sushmitaku <ksushmita1999@gmail.com>
2020-09-29 15:29:11 +0530

    Added: code refactor

--- AddressBook.java
+++ AddressBook.java
@@ -1,85 +1,123 @@
+package com.company;
 import java.util.*;
-class AdderssBook
-{
-    public static void main(String args[])
-   {
-        ArrayList<String> firstName = new ArrayList<String>();
-        ArrayList<String> lastName = new ArrayList<String>();
-        Map<String, String> cityState = new HashMap<String, String>();
-        ArrayList<String> address = new ArrayList<String>();
-        ArrayList<Integer> zipCode = new ArrayList<Integer>();
-        ArrayList<String> phoneNumber = new ArrayList<String>();
-        System.out.println("Welcome to Address Book Creation");
-        System.out.println("Enter How many person address you want to store");
+import java.lang.*;
+public class AddressBook {
+    public static void main(String args[]) {
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        System.out.println("Welcome to Address Book");
+        Operation object=new Operation();
         Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        for (int i = 1; i <= number; i++)
-	{
-        	System.out.println("Enter details of person" + i);
-            	input.nextLine();
-            	System.out.println("Enter First Name");
-            	firstName.add(input.nextLine());
-            	input.nextLine();
-            	System.out.println("Enter Last Name");
-            	lastName.add(input.nextLine());
-            	System.out.println("Enter Address ");
-            	address.add(input.nextLine());
-            	System.out.println("Enter City and State");
-            	cityState.put(input.nextLine(), input.nextLine());
-            	System.out.println("Enter zip code");
-            	zipCode.add(input.nextInt());
-            	input.nextLine();
-            	System.out.println("Enter PhoneNumber");
-            	phoneNumber.add(input.nextLine());
+        while (true) {
+            System.out.println("Enter 1 to create address");
+            System.out.println("Enter 2 to edit details of person ");
+            System.out.println("Enter 3 to delete person");
+            System.out.println("Enter 4 to sort by First Name");
+            System.out.println("Enter 5 to sort by zipcode");
+
+            switch (input.nextInt()) {
+                case 1:
+                    object.createAddress();
+                    break;
+                case 2:
+                    object.editAddress();
+                    break;
+                case 3:
+                    object.deleteAddress();
+                    break;
+                case 4:
+                    object.sortByFirstName();
+                    break;
+                default:
+                    System.exit(0);
+                    break;
+            }
+        }
+
+    }
+}
+class Operation
+{
+    HashMap<String,ArrayList<String>> map = new HashMap<String,ArrayList<String>>();
+    Scanner input=new Scanner(System.in);
+    String firstName,lastName,address,city,state,zipCode,phoneNumber;
+    public void createAddress()
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        System.out.println("Enter First Name");
+        firstName=(input.nextLine());
+        System.out.println("Enter Last Name");
+        list.add(input.nextLine());
+        System.out.println("Enter Address ");
+        list.add(input.nextLine());
+        System.out.println("Enter City");
+        list.add(input.nextLine());
+        System.out.println("Enter state");
+        list.add(input.nextLine());
+        System.out.println("Enter zip code");
+        list.add(input.nextLine());
+        System.out.println("Enter phoneNumber code");
+        list.add(input.nextLine());
+        map.put(firstName,list);
+        System.out.println(map);
+
+    }
+ 	public void editAddress()
+    	{
+		ArrayList<String> list = new ArrayList<String>();
+        	System.out.println("Enter name whose address you want to edit");
+        	String name=input.nextLine();
+		System.out.println("enter what you want to edit");
+		String edit=input.nextLine();
+		System.out.println("Enter final change you want");
+		String finalValue=input.nextLine();list.add(finalValue);
+        	for(Map.Entry entry:map.entrySet())
+        	{
+            		if(map.containsKey(name))
+            		{
+                		map.replace(name,list);
+                		System.out.println(map);
+            		}
+            		else
+            		{
+                		System.out.println("sorry Name do not exist in AddressBook");
+            		}
+        	}
     	}
-        AddressBookOperation object=new AddressBookOperation();
-        object.displayAddressBook(firstName);
-        object.displayAddressBook(lastName);
-        object.displayAddressBook(address);
-        object.displayCityState(cityState);
-        System.out.println(zipCode);
-        object.displayAddressBook(phoneNumber);
-        System.out.println("press 1 to sort by lastName or press 2 sort by zipcode");
-        switch(input.nextInt())
-        {
-           	case 1:object.sortByLastName(lastName);
-           	break;
-           	case 2:object.sortByzipCode(zipCode);
-           	break;
-            	default:System.out.println("Invalid input");
-            	break;
-        }
-        System.out.println("visit again to store more address");
-   }
-}
-class AddressBookOperation implements AddressBuilder
-{
-    public  void displayCityState( Map<String, String>cityState)
-    {
-        for(Map.Entry entry:cityState.entrySet())
-        {
-          	System.out.println(entry.getKey()+"   "+entry.getValue()+" ");
+	public void deleteAddress()
+	{
+		try
+		{
+        		System.out.println("Enter name whose address you want to delete");
+        		String name=input.nextLine();
+        		for(Map.Entry entry:map.entrySet())
+        		{
+            			if(map.containsKey(name))
+            			{
+                			map.remove(name);
+                			System.out.println(map);
+            			}
+            			else
+            			{
+                			System.out.println("sorry Name do not exist in AddressBook");
+            			}
+        		}
+		}
+		catch(Exception e)
+		{
 
-        }
+		}
+	}
+	public void sortByFirstName()
+	{
+        	// TreeMap to store values of HashMap
+        	TreeMap<String, ArrayList<String>> sorted = new TreeMap<>();
+        	// Copy all data from hashMap into TreeMap
+        	sorted.putAll(map);
+        	// Display the TreeMap which is naturally sorted
+        	map.putAll(sorted);
+        	System.out.println(sorted);
+ 	}
 
-    }
-    public void displayAddressBook(ArrayList<String> list)
-    {
-        System.out.println(list);
-    }
-    public  void sortByLastName(ArrayList<String> list)
-    {
-        Collections.sort(list);
-        System.out.println(list);
-    }
-    public static void sortByzipCode(ArrayList<Integer> list)
-    {
-        Collections.sort(list);
-        System.out.println(list);
-    }
 
 }
-interface AddressBuilder
-{
-    abstract void displayCityState(Map<String, String>cityState);
-}
+
